552d21beb7415599d819dd9eac22154e79db9e17
zjuchenyuan <chenyuan@zju.edu.cn>
2020-08-04 18:14:56 +0800

    add forecast

--- run.py
+++ run.py
@@ -27,15 +27,18 @@ def getdata(coin, page=1):
         return pickle.load(open("__pycache__/"+coin+page, "rb"))
     data = [Decimal(i['final_funding_rate']) for i in sess.get("https://futures.huobi.com/swap-order/x/v1/swap_funding_rate_page?contract_code="+coin+"-USD&page_index="+page+"&page_size=100", headers={"source":"web"}).json()["data"]["settle_logs"]]
     settle = [Decimal(i["instrument_info"][0]["settle_price"]) for i in sess.get("https://futures.huobi.com/swap-order/x/v1/swap_delivery_detail?symbol="+coin+"&page_index="+page+"&page_size=100", headers={"source":"web"}).json()["data"]["delivery"]]
+    nextdata = sess.get("https://futures.huobi.com/swap-order/x/v1/swap_funding_rate?contract_code="+coin+"-USD", headers={"source":"web"}).json()["data"]
+    next1, next2 = Decimal(nextdata["final_funding_rate"]), Decimal(nextdata["funding_rate"])
     if sum(data[:3])<0:
         warns += 1
         status = "warning "+str(warns)
+    res = [data, settle, next1, next2]
     if USECACHE:
-        open("__pycache__/"+coin+page, "wb").write(pickle.dumps([data,settle]))
-    return data, settle
+        open("__pycache__/"+coin+page, "wb").write(pickle.dumps(res))
+    return res
 
 def calcprofit(coin, days, yearly=True, returndata=False):
-    fulldata, fullsettle = getdata(coin)
+    fulldata, fullsettle, next1, next2 = getdata(coin)
     data, settle = fulldata[:days*3], fullsettle[:days*3]
     profit_coin = sum([k/settle[i] for i,k in enumerate(data)]) #1美元在结算中能挣到多少币
     profit_usd = profit_coin*settle[0] #按最近一次结算价格 这些挣到的币现在值多少USD
@@ -47,7 +50,7 @@ def calcprofit(coin, days, yearly=True, returndata=False):
     if not yearly:
         if returndata:
             return profit_usd
-        return "%.2f"%(profit_usd*100)+ "%"
+        return "%.2f‰"%(profit_usd*1000)
     #return "%.2f"%(sum(data)/len(data)*3*365*100) + "%"+suffix
     return "%6.2f"%(profit_usd/len(data)*3*365*100) + "%"+suffix
 
@@ -90,13 +93,14 @@ if __name__ == "__main__":
             print("",i, profit, length,"", sep="|")
         exit()
     from pprint import pprint
-    text = "|币种|昨日 |7日年化|月年化|\n"
+    text = "|币种| 预测 | 昨日 |7日年化|月年化|\n"
     t = []
     for coin in COINLIST:
         t.append(["|".join(
             [
                 "",
                 coin+(" " if len(coin)==3 else ""),
+                "%.2f‰"%((getdata(coin)[2]+getdata(coin)[3])*1000),
                 calcprofit(coin,1, yearly=False),
                 calcprofit(coin,7),
                 calcprofit(coin,30),
@@ -114,9 +118,9 @@ if __name__ == "__main__":
     
     t = []
     for coin in ALLCOINS:
-        t.append([coin+(" " if len(coin)==3 else ""), calcprofit(coin,1, yearly=False), calcprofit(coin,7), calcprofit(coin,30), calcprofit(coin,1, yearly=False, returndata=True)])
+        t.append([coin+(" " if len(coin)==3 else ""), "%.2f‰"%((getdata(coin)[2]+getdata(coin)[3])*1000), calcprofit(coin,1, yearly=False), calcprofit(coin,7), calcprofit(coin,30), calcprofit(coin,1, yearly=False, returndata=True)])
     t.sort(key=lambda i:i[-1])
-    html = """<!doctype html><meta charset="utf-8">\n数据更新时间：%s<br>\n<table><thead><tr><th>币种</th><th>昨日收益</th><th>7日年化</th><th>30日年化</th></tr></thead><tbody>\n"""%(time.strftime("%Y-%m-%d %H:%M:%S"))
+    html = """<!doctype html><meta charset="utf-8">\n数据更新时间：%s<br>\n<table><thead>\n<tr><th>币种</th><th>预测收益</th><th>昨日收益</th><th>7日年化</th><th>30日年化</th></tr></thead><tbody>\n"""%(time.strftime("%Y-%m-%d %H:%M:%S"))
     for data in t:
         html += "<tr><td>" + "</td><td>".join(data[:-1]) + "</td></tr>\n"
     html += "</tbody></table><blockquote>* 这些币种上线不足30日</blockquote>"
