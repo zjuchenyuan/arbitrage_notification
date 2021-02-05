ea26c47809dd409aa34aef837791cd233b3e6797
zjuchenyuan <chenyuan@zju.edu.cn>
2021-02-05 22:42:33 +0800

    also query bag and mdx

--- liquidation_alert.py
+++ liquidation_alert.py
@@ -13,3 +13,16 @@ for flag, data in {1:WATCHER_UP, -1:WATCHER_DOWN}.items():
         if diff<0.1:
             send(coin+" "+note+" 爆仓预警", str(nowprice).rstrip("0")+"->"+str(price)+" "+"%.2f%%"%(diff*100))
 
+import sys
+sys.path.append("/home/api.chenyuan.me")
+from qieman import *
+
+ht_price, bag_price = graphql_token_price("0xa042fb0e60125a4022670014ac121931e7501af4", HECO_GRAPH)
+ht_price, bags_price = graphql_token_price("0x6868d406a125eb30886a6dd6b651d81677d1f22c", HECO_GRAPH)
+if bag_price<0.7 or bags_price<150:
+    send("bag已经暴跌", "BAG %(bag_price).3f BAGS %(bags_price).2f"%(locals()))
+if bag_price>1:
+    send("已恢复bag", "BAG %(bag_price).3f BAGS %(bags_price).2f"%(locals()))
+ht_price, mdx_price = graphql_token_price("0x25d2e80cb6b86881fd7e07dd263fb79f4abe033c", HECO_GRAPH)
+if mdx_price < 2.21:
+    send("mdx跌幅大于30%", "MDX %(mdx_price).3f"%(locals()))
